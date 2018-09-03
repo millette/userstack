e06671895c8b7b26fde9b51fbb53c36db887691b
Robin Millette <robin@millette.info>
2018-09-03 18:02:16 -0400

    My current 7z (9.2.0 on Debian) doesn't support stdin.

--- 7f.js
+++ 7f.js
@@ -1,13 +1,26 @@
 'use strict'
 
+// https://archive.org/download/stackexchange/stackoverflow.com-Users.7z
+
 // core
 const { spawn } = require('child_process')
 
 // npm
 const stdout = require('stdout')
 const xml = require('xml-flow')
+// const got = require('got')
+
+// const input = got.stream('https://archive.org/download/stackexchange/stackoverflow.com-Users.7z')
 
 const ls = spawn('7z', ['x', '-so', '/home/millette/so/official/stackoverflow.com-Users.7z'])
+// const ls = spawn('7z', ['x', '-so', '-si'])
+// { stdio: ['pipe'] }
+
+// my 7z can't handle stdin
+// const inp = got.stream('https://archive.org/download/stackexchange/stackoverflow.com-Users.7z')
+
+// inp.pipe(ls.stdin)
+
 const oy = xml(ls.stdout)
 const std = stdout()
 
@@ -15,4 +28,30 @@ oy.on('tag:row', (row) => {
   delete row.$name
   std.write(JSON.stringify(row))
   ls.kill()
+})
+
+
+ls.on('close', () => {
+  console.error('ls close:')
+})
+
+oy.on('close', () => {
+  console.error('oy close:')
+})
+
+inp.on('close', () => {
+  console.error('inp close:')
+})
+
+
+ls.on('error', (er) => {
+  console.error('ls error:', er)
+})
+
+oy.on('error', (er) => {
+  console.error('oy error:', er)
+})
+
+inp.on('error', (er) => {
+  console.error('inp error:', er)
 })
